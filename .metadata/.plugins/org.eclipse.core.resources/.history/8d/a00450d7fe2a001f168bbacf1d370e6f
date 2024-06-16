d2cce8fa567cb5ebd5b5412d5c1e3d1aeb00b78d
Qa-Prakash <qa.prakash97@gmail.com>
2024-06-16 14:16:02 +0530

    My First commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8d/a00450d7fe2a001f168bbacf1d370e6f
@@ -0,0 +1,5 @@
+package com.ecom.qa.util;
+
+public class TestUtil {
+
+}
---
+++ Ecommerce/src/main/java/com/ecom/qa/util/TestUtil.java
@@ -0,0 +1,7 @@
+package com.ecom.qa.util;
+
+public class TestUtil {
+	public final  static long IMPLICIT_WAIT = 20;
+	public final static long PAGE_TIMEOUT = 20;
+
+}
